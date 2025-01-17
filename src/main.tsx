b31923ce6471861d969234553e07613b1e76f5fb
maoyln <maoyl_yx@163.com>
2025-01-17 15:17:43 +0800

    zustand-订阅模式-subscribe

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,9 @@ import { createRoot } from 'react-dom/client'
 // import App from './App01.tsx' // zustand的基本使用和异步（步骤较为繁琐）
 // import App from './App02.tsx' // immer的使用简化zustand的步骤
 // import App from './App03.tsx' // immer的使用简化zustand的步骤使用store的简化操作
-import App from './App05.tsx' // devtools的使用
+// import App from './App04.tsx' // devtools的使用
+// import App from './App05.tsx' // 持久化保存
+import App from './App06.tsx' // 发布订阅
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
